c3b73a05fc6a8a8cac70fdefb8301a36f4e6ba18
Matheus Gomes <maatheus.gsr@gmail.com>
2022-12-21 18:07:05 -0300

    browser-fs-access functions, filehandle and md useStates hooks added

--- src/context/FileContext.jsx
+++ src/context/FileContext.jsx
@@ -1,26 +1,62 @@
 import React from "react";
+import {
+  fileOpen,
+  fileSave,
+  supported,
+} from "https://unpkg.com/browser-fs-access";
 
 export const FileContext = React.createContext();
 
 export const FileStorage = ({ children }) => {
-  const [fileResult, setFileResult] = React.useState("");
+  const [fileHandle, setFileHandle] = React.useState();
   const [fileName, setFileName] = React.useState("");
+  const [markdown, setMarkdown] = React.useState();
 
-  const readFile = () => {
-    const input = document.querySelector('input[type="file"]');
-    const file = input.files[0];
-    const reader = new FileReader();
-    reader.fileName = file.name;
-    reader.readAsText(file);
-    reader.onload = () => {
-      setFileResult(reader.result);
-      setFileName(reader.fileName);
-    };
+  const options = {
+    fileName: fileName,
+    description: "Markdown/Text file",
+    extensions: [".md", ".txt"],
+  };
+
+  const openFile = async () => {
+    try {
+      const file = await fileOpen({
+        description: "Markdown/Text file",
+        mimeTypes: ["text/markdown", "text/plain"],
+        extensions: [".md"],
+        excludeAcceptAllOption: true,
+      });
+      setFileHandle(file.handle);
+      setFileName(file.name);
+      const content = await file.text();
+      setMarkdown(content);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
+  // Blob para salvar o conteudo escrito
+  const blob = new Blob([markdown], { type: "text/markdown" });
+
+  const save = async () => {
+    try {
+      await fileSave(blob, options, fileHandle);
+    } catch (e) {
+      console.log(e.message);
+    }
   };
 
   return (
     <FileContext.Provider
-      value={{ fileResult, setFileResult, fileName, setFileName, readFile }}
+      value={{
+        fileName,
+        setFileName,
+        markdown,
+        setMarkdown,
+        openFile,
+        save,
+        supported,
+      }}
     >
       {children}
     </FileContext.Provider>
