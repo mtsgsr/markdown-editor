d8598f191912e6b9e513fa92ecda124c8c3adfc0
Matheus Gomes <maatheus.gsr@gmail.com>
2022-12-22 23:41:04 -0300

    new colors added to both themes

--- src/styles/themes.js
+++ src/styles/themes.js
@@ -1,17 +1,27 @@
 export const lightTheme = {
   background: "#FFFFFF",
   header: "#2c2d31",
+  separator: "#444",
   headings: "#f5f5f5",
-  text: "#2C2738",
-  accent: "#df6943",
+  title: "#eee",
+  text: "#24292f",
+  accent: "#e56643", // df6943
   menu: "#feffff",
+  border: "#ccc",
+  hover: "rgba(255, 255, 255, 0.1)",
+  hoverEye: "rgba(0, 0, 0, 0.1)",
 };
 
 export const darkTheme = {
   background: "#15161a",
   header: "#2c2d31",
+  separator: "#444",
   headings: "#1e1f23",
+  title: "#eee",
   text: "#D6E1DF",
-  accent: "#df6943",
+  accent: "#e56643",
   menu: "#feffff",
+  border: "#333",
+  hover: "rgba(255, 255, 255, 0.1)",
+  hoverEye: "rgba(255, 255, 255, 0.1)",
 };
